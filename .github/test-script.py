d685614dddf38cf574fd14327efacd81bef06765
Frank Hessel <fhessel@seemoo.tu-darmstadt.de>
2021-02-25 20:18:38 +0100

    Add GitHub Action to build the framework on different Ubuntus (#2)

---
+++ .github/test-script.py
@@ -0,0 +1,8 @@
+# This is just a dummy script used in the GitHub Actions to assure that the
+# chirpotle.sh script
+# command runs successfully.
+
+from chirpotle.context import tpy_from_context
+tc, devices = tpy_from_context()
+
+# exit 0
