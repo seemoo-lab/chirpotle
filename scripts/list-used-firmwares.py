5372c6cb6674971f2396f5bb842eda44712c705a
Frank Hessel <fhessel@seemoo.tu-darmstadt.de>
2022-12-02 11:59:05 +0100

    Add chirpotle.sh flash command

--- scripts/list-used-firmwares.py
+++ scripts/list-used-firmwares.py
@@ -6,6 +6,19 @@ import sys
 # Returns a list of unique firmware configurations for a given confname
 # Call: list-used-firmwares.py <firmwares|platforms> confdir confname
 
+# Returns the platform for a specific firmware:
+# Call: list-used-firmwares.py platform-for <firmware>
+
+# Map firmwares to platforms
+pmap = {
+    "esp32-generic": "esp32",
+    "lora-feather-m0": "arm_none",
+    "lopy4-uart": "esp32",
+    "lopy4": "esp32",
+    "t-beam-uart": "esp32",
+    "native-raspi": "arm_linux",
+}
+
 def conf_path_to_obj(d):
     """
     Read configuration files from an input directory d into an object
@@ -18,43 +31,42 @@ def conf_path_to_obj(d):
         configs[cfgname] = c
     return configs
 
-if sys.argv[1] not in ["firmwares", "platforms"]:
-    print("First parameter must be \"firmwares\" or \"platforms\"")
-    exit(1)
+if sys.argv[1] in ["firmwares", "platforms"]:
 
-confdir = pathlib.Path(sys.argv[2])
-hostconf = conf_path_to_obj(confdir / "hostconf")[pathlib.Path(sys.argv[3]).stem]
-nodeconfs = conf_path_to_obj(confdir / "nodeconf")
+    confdir = pathlib.Path(sys.argv[2])
+    hostconf = conf_path_to_obj(confdir / "hostconf")[pathlib.Path(sys.argv[3]).stem]
+    nodeconfs = conf_path_to_obj(confdir / "nodeconf")
 
-# Find all configs that are used
-used_nodeconfs = set()
-for sec in hostconf.sections():
-    if "conf" in hostconf[sec]:
-        used_nodeconfs.add(hostconf[sec]["conf"])
-used_confs = map(lambda confname: nodeconfs[confname],
-                 [pathlib.Path(c).stem for c in used_nodeconfs])
+    # Find all configs that are used
+    used_nodeconfs = set()
+    for sec in hostconf.sections():
+        if "conf" in hostconf[sec]:
+            used_nodeconfs.add(hostconf[sec]["conf"])
+    used_confs = map(lambda confname: nodeconfs[confname],
+                    [pathlib.Path(c).stem for c in used_nodeconfs])
 
-# Find all firmwares
-used_firmwares = set()
-for conf in used_confs:
-    for sec in conf.sections():
-        if "firmware" in conf[sec]:
-            used_firmwares.add(conf[sec]["firmware"])
+    # Find all firmwares
+    used_firmwares = set()
+    for conf in used_confs:
+        for sec in conf.sections():
+            if "firmware" in conf[sec]:
+                used_firmwares.add(conf[sec]["firmware"])
 
-if sys.argv[1] == "firmwares":
-    print(" ".join(used_firmwares))
+    if sys.argv[1] == "firmwares":
+        print(" ".join(used_firmwares))
+    else:
+        used_platforms = set()
+        for fw in used_firmwares:
+            if fw in pmap:
+                used_platforms.add(pmap[fw])
+        print(" ".join(used_platforms))
+elif sys.argv[1] == "platform-for":
+    if sys.argv[2] in pmap:
+        print(pmap[sys.argv[2]])
+        exit(0)
+    else:
+        print("unknown firmware:", sys.argv[2], file=sys.stderr)
+        exit(1)
 else:
-    # Map firmwares to platforms
-    pmap = {
-        "esp32-generic": "esp32",
-        "lora-feather-m0": "arm_none",
-        "lopy4-uart": "esp32",
-        "lopy4": "esp32",
-        "t-beam-uart": "esp32",
-        "native-raspi": "arm_linux",
-    }
-    used_platforms = set()
-    for fw in used_firmwares:
-        if fw in pmap:
-            used_platforms.add(pmap[fw])
-    print(" ".join(used_platforms))
+    print("First parameter must be \"firmwares\", \"platforms\" or \"platform-for\".")
+    exit(1)
